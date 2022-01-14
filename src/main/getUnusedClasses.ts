92b5c53c5feb78981dcaac209a8d8c40f42d43b1
Blazh <iblazevi@gmail.com>
2022-01-14 14:52:51 +0100

    Fix linting

--- src/main/getUnusedClasses.ts
+++ src/main/getUnusedClasses.ts
@@ -5,26 +5,26 @@ import findUnusedCss from './findUnusedCss';
 
 export default class UnusedClasses {
   private allHtmlContent = '';
-  
+
   constructor(private styleExtension: string) {}
 
-  getUnusedClasses (projectPath: string): Promise<[[string[], string]]> {
+  getUnusedClasses(projectPath: string): Promise<[[string[], string]]> {
     const list = findHtml(projectPath);
 
     return this.mapClasses(list).then((r) => {
       return r.filter((c) => {
         const unusedCssClasses: string[] | string = c?.length ? c[0] : [];
-        return unusedCssClasses && unusedCssClasses.length > 0
+        return unusedCssClasses && unusedCssClasses.length > 0;
       });
     }) as Promise<[[string[], string]]>;
   }
 
-  getGlobalUnusedClasses (globalStyles: string) {
+  getGlobalUnusedClasses(globalStyles: string) {
     const classes = findUnusedCss(this.allHtmlContent, globalStyles);
     return classes;
   }
 
-  private mapClasses (list: string[]) {
+  private mapClasses(list: string[]) {
     const promiseArray = list.map((element) => {
       const htmlPath = element;
       const htmlContent = fs.readFileSync(htmlPath, 'utf8');
